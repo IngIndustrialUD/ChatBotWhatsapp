51bb12b74ca269eb029955862652af3e67f26570
IngIndustrialUD <ingindudistrital@gmail.com>
2026-04-22 20:25:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -517,7 +517,7 @@ R_CONTACTOS = (
 R_CAMBIOESTUDIOS = (
     "*Cambio de Plan de estudios*\n\n"
     "Puedes consultar el cambio de pensum en el siguiente enlace:\n"
-    f"*{LINK_PLANESTUDIOS}*\n\n"
+    f"{LINK_PLANESTUDIOS}\n\n"
     "*Al seleccionar cada asignatura, podrás acceder al syllabus correspondiente*\n\n"
     "También encontrarás información del nuevo plan de estudios, incluyendo la herramienta "
     "en excel y el formato de autorización para el cambio del plan de estudios, en el "
